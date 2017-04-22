2b6606fc1936b2a41b38df3455ac22268503b175
victor <vpalmacarrasco@gmail.com>
2017-04-22 17:02:34 +0200

    lazy

--- app/src/main/java/com/iplusd/victor/myplayer/MainActivity.kt
+++ app/src/main/java/com/iplusd/victor/myplayer/MainActivity.kt
@@ -2,33 +2,22 @@ package com.iplusd.victor.myplayer
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ListView
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val f: (Int) -> Int = { x -> x * x }
-
+    val recycler by lazy { find<RecyclerView>(R.id.recycler) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recycler.adapter = MediaAdapter(fetchMedia()) {
-//            item -> showMyToast(item.title)
-            showMyToast(it.title)
-        }
-
-
-        val textView = TextView(this).apply2 {
-            textSize = 20f
-        }
-
-        val viewGroup: ViewGroup = FrameLayout(this)
-        val list: List<View> = viewGroup.filterViews { view -> view.visible }
+        recycler.adapter = MediaAdapter() //---> se setea adapter cuando se inicializa, no antes!
     }
 
 
