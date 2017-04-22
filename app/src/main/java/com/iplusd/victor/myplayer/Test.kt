7e0acd589e3c43fb10b65d285e79803f6ea68b00
victor <vpalmacarrasco@gmail.com>
2017-04-22 13:32:41 +0200

    bucles

--- app/src/main/java/com/iplusd/victor/myplayer/Test.kt
+++ app/src/main/java/com/iplusd/victor/myplayer/Test.kt
@@ -11,23 +11,36 @@ import android.widget.TextView
 
 
 fun test (view:View) {
-    when (view) {
-        is TextView -> print(view.text)
-        is ViewGroup -> print("children: ${view.childCount}")
-    }
-
     val int = 10
+
+    val list = listOf<Int>(1, 2, 3)
+
+    for (item in list) {}
+    // rangos
+    for (i in 1..10) {} // hasta 10
+    for (i in 1 until 10) {} // hasta 9
+
     when (int) {
         in 5..20 -> ""
         0 -> ""
         1, 3, 5 -> ""
     }
 
+    when (view) {
+        is TextView -> print(view.text)
+        is ViewGroup -> print("children: ${view.childCount}")
+    }
+
+    when {
+        view is TextView -> print(view.text)
+        System.currentTimeMillis() > 0 -> ""
+        int in 30..40 -> print("")
+    }
 
     val result:Any = when (int) {
         in 5..20 -> ""
         0 -> 2
         1, 3, 5 -> ""
-        else -> ""
+        else -> IllegalStateException("No es un valor válido")
     }
 }
